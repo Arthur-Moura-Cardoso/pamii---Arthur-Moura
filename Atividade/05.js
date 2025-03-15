891635fedb2f6ae54161a44207dcd87892bf3522
Arthur Moura <arthurmoura.2007.ti@gmail.com>
2025-03-15 17:26:12 -0300

    Add files via upload

---
+++ Atividade/05.js
@@ -0,0 +1,12 @@
+let palavra = "Corinthians"
+let letra = "i"
+let quantidade = 0
+
+for (var i = 0; i < palavra.length; i++) {
+    if (palavra[i] == letra) {
+      quantidade++
+    }
+}
+
+console.log(quantidade)
+  
